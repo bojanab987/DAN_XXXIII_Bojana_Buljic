9a0e4361eccf89bbb3f972da74255238937388fb
bojanabb <bojana.buljic@gmail.com>
2020-06-17 09:44:15 +0200

    Methods Reading from FileByThread_1.txt and from FileByThread_22.txt implemented. In Main created Thread array and initialized 4 Threads

--- DAN_XXXIII_Bojana_Buljic/Zadatak_1/Program.cs
+++ DAN_XXXIII_Bojana_Buljic/Zadatak_1/Program.cs
@@ -20,25 +20,7 @@ namespace Zadatak_1
         /// <returns></returns>
         static int[,] IdentityMatrix()
         {
-
-            int[,] result = new int[100, 100];
-
-            using (StreamWriter sw = new StreamWriter(file1))
-            {
-                for (int i = 0; i < 100; ++i)
-                {
-                    for (int j = 0; j < 100; ++j)
-                    {
-                        if (i == j)
-                            sw.Write(result[i, j] = 1);
-
-                        else
-                            sw.Write(result[i, j] = 0);
-                    }
-                    sw.WriteLine();
-                }
-            }
-            return result;
+            return new int[100, 100];
 
         }
 
@@ -47,36 +29,75 @@ namespace Zadatak_1
         /// </summary>
         static void GetRandomNumbers()
         {
-            long[] oddNums = new long[1000];
-            Random rnd = new Random();
-            long num = 0;
-            for (int i = 0; i < oddNums.Length; i++)
+
+            
+        }
+
+        /// <summary>
+        /// Method for Reading Identity Matrix from FileByThread_1.txt and printing it on Console
+        /// </summary>
+        static void ReadFileByThread1()
+        {
+            string[] readFile1 = File.ReadAllLines(file1);
+            foreach (string s in readFile1)
             {
-
-                do
-                {
-                    num = rnd.Next(0, 10000);
-                    oddNums[i] = num;
-
-                } while (num % 2 == 0);
-
-
+                Console.WriteLine(s);
             }
 
-            using (StreamWriter sw = new StreamWriter(file2))
+        }
+
+        /// <summary>
+        /// Method for Reading FileByThread_22.txt
+        /// </summary>
+        static void ReadFileByThread2()
+        {
+            string[] readFile2 = File.ReadAllLines(file2);
+
+            long sum = 0;
+            foreach (string s in readFile2)
             {
-
-                for (int i = 0; i < oddNums.Length; i++)
-                {
-
-                    sw.WriteLine(oddNums[i]);
-                }
+                sum += Convert.ToInt64(s);
             }
+
+            Console.WriteLine("The sum of all odd numbers:" + sum);
         }
 
         static void Main(string[] args)
         {
+            Thread[] threadsArray = new Thread[4]
+           {
+                new Thread(() => IdentityMatrix()),
+                new Thread(() => GetRandomNumbers()),
+                new Thread(() => ReadFileByThread1()),
+                new Thread(() => ReadFileByThread2())
+           };
+            for (int i = 0; i < 4; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    threadsArray[i].Name = string.Format("Thread_{0}{1}", i + 1, i + 1);
+                }
+                else
+                {
+                    threadsArray[i].Name = string.Format("Thread_{0}", i + 1);
+                }
+                Console.WriteLine(threadsArray[i].Name + " is created");
+            }            
 
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            threadsArray[0].Start();
+            threadsArray[1].Start();
+            threadsArray[0].Join();
+            threadsArray[1].Join();
+            sw.Stop();
+            Console.WriteLine("\n{0} milliseconds\n", sw.ElapsedMilliseconds);
+            Thread.Sleep(1000);
+
+            threadsArray[2].Start();
+            threadsArray[3].Start();
+
+            Console.ReadLine();
         }
     }
 }
